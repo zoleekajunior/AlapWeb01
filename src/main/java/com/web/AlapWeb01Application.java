95abe944b1e6523ce10e842101ae9729daf4ad18
Korponai Zoltán <korponaiz@gmail.com>
2024-01-23 22:56:17 +0100

    Alap verzió

---
+++ src/main/java/com/web/AlapWeb01Application.java
@@ -0,0 +1,13 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlapWeb01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlapWeb01Application.class, args);
+	}
+
+}
